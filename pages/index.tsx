83366bdbc6b8f8835d8a331888b419b610087a6e
oliviel <olivielultimate@gmail.com>
2022-10-02 19:12:18 -0400

    Add eslint and prettier rules

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,11 @@
 import type { NextPage } from "next";
 
-const Home: NextPage = () => {
-  return (
-    <main className="mx-auto max-w-screen-xl my-10">
-      <section className="text-center">
-        <h1 className="text-4xl">Hello web3Commerce</h1>
-      </section>
-    </main>
-  );
-};
+const Home: NextPage = () => (
+  <main className="mx-auto max-w-screen-xl my-10">
+    <section className="text-center">
+      <h1 className="text-4xl">Hello web3Commerce</h1>
+    </section>
+  </main>
+);
 
 export default Home;
